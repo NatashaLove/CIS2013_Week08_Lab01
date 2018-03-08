3e641d30cd570a334ecf9e60e209f38df1d268d3
Natasha S Love <debora@inbox.ru>
2018-03-07 19:57:14 -0600

    out ofstream

---
+++ CIS2013_Week08_Lab01.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main () {
+	int number=0;
+	char cont = 'y';
+	ofstream out_a;
+	
+	// out_a - object; open = method; Opens location to store data
+	out_a.open("numbers.dat");
+	
+	if (out_a.fail()){
+		cout << "Unable to open file numbers.dat" << endl;
+		
+		//stops the program
+		exit (1);
+	}
+	
+	
+	while(cont == 'y'){
+		cout << "Type a number to save to the file: ";
+		cin >> number;
+		
+		// adds space between output numbers
+		out_a << number << " ";
+		
+		cout << "Type another number (y/n):";
+		cin >> cont;
+		
+	}
+	out_a.close()
+	return 0;
+}
